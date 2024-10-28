69d97f16e97a7c07f96fd682b864d0b467a96d5e
pedrocruz <JayJMarquez05@gmail.com>
2024-10-28 18:14:34 -0500

    Exercise 5 Complete.

--- modern-syntaxing.js
+++ modern-syntaxing.js
@@ -1,4 +1,4 @@
-function copyAndPasteMe() {
+function copyAndPasteBin() {
   console.log(
     `
     ===========================================
@@ -12,6 +12,12 @@ function copyAndPasteMe() {
   console.log(
     `
     -=|{Break in between example & exercise.}|=-
+    `
+  );
+
+  console.log(
+    `
+    -=|{---}|=-
     `
   );
 
@@ -314,15 +320,89 @@ function exercise4() {
   );
 }
 
-function exercise5() {}
+function exercise5() {
+  console.log(
+    `
+    ===========================================
+    ========[New Render for Exercise 5]========
+    ===========================================
+    `
+  );
+
+  // ----------------------------------------------------------------
+
+  // const originalObject = {
+  //   foo: "Hello",
+  //   bar: 100,
+  // };
+
+  // const clonedObject = { ...originalObject };
+  // console.log("Clone: ", clonedObject); // { foo: 'Hello', bar: 100 }
+
+  // const originalObject = {
+  //   foo: "Hello",
+  //   bar: 100,
+  // };
+
+  // const clonedObject = originalObject;
+  // clonedObject.foo = "Goodbye";
+
+  // console.log(originalObject); // { foo: 'Goodbye', bar: 100 }
+
+  const originalObject = {
+    foo: "Hello",
+    bar: 100,
+  };
+
+  // Copy the properties of originalObject:
+  const clonedObject = { ...originalObject };
+
+  // Update the properties of clonedObject:
+  clonedObject.foo = "Goodbye";
+  clonedObject.bar = 0;
+
+  console.log("Original: ", originalObject); // { foo: 'Hello', bar: 100 }
+  console.log("Clone: ", clonedObject); // { foo: 'Goodbye', bar: 0 }
+
+  console.log(
+    `
+    -=|{Break in between example & exercise.}|=-
+    `
+  );
+
+  const friendPCSpecs = {
+    ram: `32gb DDR4`,
+    cpu: `Ryzen 7 5800X`,
+    gpu: `Nvidia RTX 3080`,
+    system: `Windows 11`,
+  };
+
+  const mySpecs = { ...friendPCSpecs };
+
+  mySpecs.ram = `16GB RGB DDR5 6400MT/s`;
+  mySpecs.cpu = `AMD Ryzen 5 7600X`;
+  mySpecs.system = `Windows 10`;
+
+  console.log(`My buddy's computer specs`, friendPCSpecs);
+
+  console.log(
+    `
+    -=|{---}|=-
+    `
+  );
+
+  console.log(`My personal specs`, mySpecs);
+}
+
+function exercise6() {}
 
 function run() {
   // exercise1();
   // exercise2();
   // exercise3();
   // exercise4();
-  exercise5();
-  // exercise6;
+  // exercise5();
+  exercise6;
   // exercise7;
   // exercise8;
   // exercise9;
