b698b85a80f88debf2036000b5a696fddc93f355
pedrocruz <JayJMarquez05@gmail.com>
2024-11-01 20:13:10 -0500

    Exercise 9 compelted.

--- exerciseFolders/exercise9.js
+++ exerciseFolders/exercise9.js
@@ -1,3 +1,49 @@
-function exercise9() {}
+function exercise9() {
+  console.log(
+    `
+    ===========================================
+    ========[New Render for Exercise 9]========
+    ===========================================
+    `
+  );
+
+  //   const age = 22;
+  //   let access;
+
+  //   if (age > 21) {
+  //     access = "Yes";
+  //   } else {
+  //     access = "No";
+  //   }
+
+  //   console.log(access); // 'Yes'
+
+  const age = 22;
+  let access = age > 21 ? "Yes" : "No";
+
+  console.log(access); // 'Yes'
+
+  console.log(
+    `
+    -=|{Break in between example & exercise.}|=-
+    `
+  );
+
+  // Convert the following `if...else` statement in to a ternary:
+
+  //   let pizza = "tasty";
+
+  //   if (pizza === "tasty") {
+  //     console.log("yum");
+  //   } else {
+  //     console.log("yuck");
+  //   }
+
+  //   const pizza = `tasty`
+  const pizza = `disgusting`;
+
+  let jack = pizza === `tasty` ? "yum" : "yuck";
+  console.log(jack);
+}
 
 export default exercise9();
